8cec4ef508fa1a472bb4c3293b5197c8eda64169
AndreyGopko <andy-gold@mail.ru>
2017-04-10 22:35:11 +0300

    init

---
+++ data.js
@@ -0,0 +1,15 @@
+const data = [
+  {
+    name: 'test1',
+    value: 23
+  },
+  {
+    name: 'test2',
+    value: 33
+  },
+  {
+    name: 'test3',
+    value: 45.88
+  },
+
+]
---
+++ script.js
@@ -0,0 +1,29 @@
+var someData = {};
+console.log($);
+
+function generateData(text) {
+  var dataLength = data.length;
+  var rand = Math.floor(Math.random() * dataLength);
+  someData = data[rand];
+  text.innerHTML = someData.name;
+}
+
+const text = document.querySelector('.req-text');
+const input = document.querySelector('.req-input');
+const btn = document.querySelector('.req-btn');
+const result = document.querySelector('.result');
+
+generateData(text);
+
+text.innerHTML = someData.name;
+btn.addEventListener('click', () => {
+  if (+input.value === someData.value) {
+    result.innerHTML = "YES! It's correct"
+    result.style.color = 'green';
+    generateData(text);
+  } else {
+    result.innerHTML = "NO! It's wrong. Try one more time";
+    result.style.color = 'red';
+  }
+  input.value = '';
+})
