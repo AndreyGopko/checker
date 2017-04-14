830ffc2e8cf4078a388d10ef1f3edc3a84867478
Andrey Gopko <AndreyGopko@users.noreply.github.com>
2017-04-14 23:23:44 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,27 +1,17 @@
 const data = [
-  {
-    name: 'test1',
-    value: 1
-  },
-  {
-    name: 'test2',
-    value: 2
-  },
-  {
-    name: 'test3',
-    value: 3
-  },
-  {
-    name: 'test4',
-    value: 4
-  },
-  {
-    name: 'test5',
-    value: 5
-  },
-  {
-    name: 'test7',
-    value: 6
-  },
-
+  {name: "ю21", value: 175}, 
+  {name: "ю22", value: 349},
+  {name: "ю23", value: 523},
+  {name: "ф2пп", value: 139},
+  {name: "ф2бп", value: 112},
+  {name: "ю11", value: 116}, 
+  {name: "ю12", value: 232},
+  {name: "ю13", value: 348},
+  {name: "ф1пп", value: 92},
+  {name: "ф1бп", value: 74},
+  {name: "з", value: 62},
+  {name: "фл", value: 1.2},
+  {name: "выезд", value: 360},
+  {name: "ар2-3", value: 180},
+  {name: "арф1", value: 90}
 ]
